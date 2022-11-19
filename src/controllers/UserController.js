373c9b14ed4a778b064a5dbebf0f454af7a9d947
Renato Silva <orenatodos@gmail.com>
2022-11-19 10:13:01 -0300

    feat: create update user method

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,6 @@
 const { randomUUID } = require('crypto')
 
-const users = require('../mocks/users')
+let users = require('../mocks/users')
 
 module.exports = {
   listUsers(request, response){
@@ -40,5 +40,29 @@ module.exports = {
     users.push(newUser)
 
     response.send(200, newUser)
+  },
+
+  updateUser(request, response){
+    const { id } = request.params
+    const { name } = request.body
+
+    const userExists = users.find((user) => user.id === id)
+
+    if(!userExists){
+      return response.send(400, { error: 'User not found' })
+    }
+
+    users = users.map((user) => {
+      if(user.id === id){
+        return {
+          ...user,
+          name
+        }
+      }
+
+      return user
+    })
+
+    response.send(200, { id, name })
   }
 }
